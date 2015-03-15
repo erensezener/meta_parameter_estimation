7211e83fa8968e43fefeb1390fb311ee80952477
Eren Sezener <erensezener@gmail.com>
2015-03-15 13:57:00 +0200

    add gamma estimation

--- agent/get_mcmc_cumulative_likelihood.m
+++ agent/get_mcmc_cumulative_likelihood.m
@@ -1,38 +1,52 @@
-function [ alpha, beta ] = get_mcmc_cumulative_likelihood(as, rs, ss)
+function [ alpha, beta, gamma ] = get_mcmc_cumulative_likelihood(as, rs, ss)
 %GET_MCMC_CUMULATIVE_LIKELIHOOD Summary of this function goes here
 %   Detailed explanation goes here
 
-alpha_beta = [0.05, 1];
-step_radius = 0.05;
+gamma_range = [0.5, 0.99];
+alpha_range = [0.001, 0.99];
+beta_range = [0.001, 5];
+step_size = 0.05;
 initial_Q = [0, 0; 0, 0];
 number_of_iterations = 5000;
-gamma = 0.9;
 
+% get range lengths
+alpha_range_length = (alpha_range(2) - alpha_range(1));
+beta_range_length = (beta_range(2) - beta_range(1));
+gamma_range_length = (gamma_range(2) - gamma_range(1));
 
-likelihood = get_action_likelihood_two_states(as, rs, ss, alpha_beta(1), alpha_beta(2), gamma, initial_Q);
+% get lower and upper bounds
+lower_bounds = [alpha_range(1), beta_range(1), gamma_range(1)];
+upper_bounds = [alpha_range(2), beta_range(2), gamma_range(2)];
+
+% get initial values
+alpha = alpha_range_length * rand(1) + alpha_range(1);
+beta = beta_range_length * rand(1) + beta_range(1);
+gamma = gamma_range_length * rand(1) + gamma_range(1);
+likelihood = get_action_likelihood_two_states(as, rs, ss, alpha, beta, gamma, initial_Q);
 
 for i = 1:number_of_iterations
     
-    %hack to obtain only positive alphas and betas
-    [alpha_prime, beta_prime] = circular_uniform(alpha_beta(1), alpha_beta(2), step_radius);
-    alpha_beta_prime = [alpha_prime, beta_prime];
+    alpha_prime = 2 * alpha_range_length * step_size * (rand(1) - 0.5) + alpha;
+    beta_prime = 2 * beta_range_length * step_size * (rand(1) - 0.5) + beta;
+    gamma_prime = 2 * gamma_range_length * step_size * (rand(1) - 0.5) + gamma;
+    primes = [alpha_prime, beta_prime, gamma_prime];
     
-    while nnz(alpha_beta_prime > [0,0]) < 2 || nnz(alpha_beta_prime < [1,5]) < 2
-        [alpha_prime, beta_prime] = circular_uniform(alpha_beta(1), alpha_beta(2), step_radius);
-        alpha_beta_prime = [alpha_prime, beta_prime];
+    % resample if alpha, beta, or gamma fall out of range
+    while nnz(primes > lower_bounds) < 3 || nnz(primes < upper_bounds) < 3
+        alpha_prime = 2 * alpha_range_length * step_size * (rand(1) - 0.5) + alpha;
+        beta_prime = 2 * beta_range_length * step_size * (rand(1) - 0.5) + beta;
+        gamma_prime = 2 * gamma_range_length * step_size * (rand(1) - 0.5) + gamma;
+        primes = [alpha_prime, beta_prime, gamma_prime];
     end
-    alpha_beta_prime = [alpha_prime, beta_prime];
-    %end hack
-
-    likelihood_prime = get_action_likelihood_two_states(as, rs, ss, alpha_beta_prime(1), alpha_beta_prime(2),gamma, initial_Q);
+        
+    likelihood_prime = get_action_likelihood_two_states(as, rs, ss, primes(1), primes(2),primes(3), initial_Q);
     if likelihood_prime < likelihood || rand(1) < exp(likelihood - likelihood_prime)
-        alpha_beta = alpha_beta_prime;
+        alpha = alpha_prime;
+        beta = beta_prime;
+        gamma = gamma_prime;
         likelihood = likelihood_prime;
     end
 end
-    
-alpha = alpha_beta(1);
-beta = alpha_beta(2);
 
 end
 
