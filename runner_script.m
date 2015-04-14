442c68936cc301277fd2a0802ad37e5b1da03028
Eren Sezener <erensezener@gmail.com>
2015-04-14 11:27:13 +0300

    add data saving

--- runner_script.m
+++ runner_script.m
@@ -7,5 +7,5 @@ number_of_states = 8*9^3 + 8 * 9^2 + 8* 9^1 + 8 * 9^0; %temporary hack
 number_of_actions = 4;
 [ alpha, beta, gamma ] = get_metaparameters(actions', rewards, states, ...
                                                 number_of_states, number_of_actions);
-                                            
-                                          
+
+save('test_run.mat');
