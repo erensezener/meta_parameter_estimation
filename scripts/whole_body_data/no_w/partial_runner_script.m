e2027490180e02d3751174e61ea989f293f0b135
Eren <erensezener@gmail.com>
2015-05-04 06:30:40 +0300

    add all data

--- scripts/whole_body_data/no_w/partial_runner_script.m
+++ scripts/whole_body_data/no_w/partial_runner_script.m
@@ -13,19 +13,19 @@ number_of_trials = 15;
 results = cell(number_of_subjects, number_of_trials);
 % results = zeros(number_of_subjects, number_of_trials);
 
-sub_no=2;
 
-% parfor sub_no = 2:number_of_subjects
-for series_no = 1:8
+parfor sub_no = 2:number_of_subjects
+  for series_no = 1:number_of_trials
     try
-        [alpha, beta, gamma] = runner_parallel_inner(sub_no, 1);
+        [alpha, beta, gamma] = runner_parallel_inner(sub_no, series_no);
         results{sub_no, series_no} = [alpha, beta, gamma];
     catch
         display(strcat('e',num2str(sub_no), ',', num2str(series_no)))
         results{sub_no, series_no} = [-1, -1, -1];
     end
 end
+end
 
 
-save('./results/whole_body/sub1_repeat_long2.mat');
+save('./results/whole_body/all_data.mat');
 
