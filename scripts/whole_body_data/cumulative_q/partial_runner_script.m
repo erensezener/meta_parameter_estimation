19c71f3e9241c4ef6b5e15ab9e6fc72558081d9e
Eren <erensezener@gmail.com>
2015-05-21 16:54:37 +0300

    add new data

--- scripts/whole_body_data/cumulative_q/partial_runner_script.m
+++ scripts/whole_body_data/cumulative_q/partial_runner_script.m
@@ -8,13 +8,13 @@ cd ..
 addpath(genpath('whole_body_data'));
 addpath(genpath('scripts/whole_body_data/cumulative_q'));
 
-number_of_subjects = 7;
+number_of_subjects = 6;
 number_of_trials = 15;
 
 results = cell(number_of_subjects, number_of_trials);
 
 %for sub_no = 2:number_of_subjects
-parfor (sub_no = 2:number_of_subjects, 6)
+parfor (sub_no = 7:7+number_of_subjects, 6)
     try
         results(sub_no,:) = get_metaparameters_of_subject(sub_no)';
         display(num2str(sub_no));
@@ -24,6 +24,6 @@ parfor (sub_no = 2:number_of_subjects, 6)
     end
 end
 
-save('./results/whole_body/constant_plus_rewards_fixed.mat');
+save('./results/whole_body/constant_plus_rewards_fixed_second_half.mat');
 
 toc
