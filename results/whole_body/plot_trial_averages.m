743540b2fed9c09475ffa78f9fbc3a06712767e3
Eren Sezener <erensezener@gmail.com>
2015-06-02 18:28:10 +0300

    add plotters

---
+++ results/whole_body/plot_trial_averages.m
@@ -0,0 +1,69 @@
+function [ means,  stdevs] = plot_trial_averages( results )
+%PLOT_TRIAL_AVERAGES Summary of this function goes here
+%   Detailed explanation goes here
+
+set(0,'DefaultFigureRenderer','opengl');
+
+num_subjects = 6;
+num_trials = 15;
+num_variables = size(results{2,2},2);
+
+means = zeros(num_trials,num_variables);
+stdevs = zeros(num_trials,num_variables);
+
+for i = 1:num_trials
+    temp = zeros(num_subjects,num_variables);
+    for j = 1:num_subjects
+        temp(j,:) = results{j,i};
+        if exp(temp(j,2)) > 1.5 && i > 3
+            j
+        end
+    end
+    means(i,:) = mean(temp(:,4));
+    stdevs(i,:) = std(temp(:,4)) / sqrt(num_subjects);
+    
+end
+
+figure;
+hold on;
+plot(means(:,1),'b-');
+plotMeanStd([1:1:num_trials],means(:,1)',stdevs(:,1)',stdevs(:,1)',[0.7 0.7 0.9],0);
+% % figure;
+% plot(means(:,2),'r:');
+% plotMeanStd([1:1:num_trials],means(:,2)',stdevs(:,2)',stdevs(:,2)',[0.9 0.7 0.7],0);
+% plot(means(:,3),'g--');
+% plotMeanStd([1:1:num_trials],means(:,3)',stdevs(:,3)',stdevs(:,3)',[0.5, 0.9 0.5],0);
+% legend('alpha mean', 'alpha SEM', 'gamma mean', 'gamma SEM', 'beta mean', 'beta SEM');
+xlim([1, 15])
+xlabel('Trial number')
+ylabel('Values')
+set(gca, 'LineWidth', 1.2)
+box on
+
+
+% figure;
+% hold on;
+% plot(means(:,1),'b-');
+% plotMeanStd([1:1:num_trials],means(:,1)',stdevs(:,1)',stdevs(:,1)',[0.7 0.7 0.9],0);
+% % figure;
+% plot(means(:,2),'r:');
+% plotMeanStd([1:1:num_trials],means(:,2)',stdevs(:,2)',stdevs(:,2)',[0.9 0.7 0.7],0);
+% legend('alpha mean', 'alpha SEM', 'gamma mean', 'gamma SEM');
+% xlabel('Trial number')
+% ylabel('Values')
+% set(gca, 'LineWidth', 1.2)
+% box on
+% 
+% 
+% figure;
+% hold on;
+% plot(means(:,3),'b-');
+% plotMeanStd([1:1:num_trials],means(:,3)',stdevs(:,3)',stdevs(:,3)',[0.7 0.7 0.9],0);
+% legend('beta mean', 'beta SEM');
+% xlabel('Trial number')
+% ylabel('Values')
+% set(gca, 'LineWidth', 1.2)
+% box on
+
+end
+
--- results/whole_body/plot_trials_3d.m
+++ results/whole_body/plot_trials_3d.m
@@ -2,17 +2,17 @@ function [] = plot_trials_3d( i, results )
 %SCATTER_PLOT_3D Summary of this function goes here
 %   Detailed explanation goes here
 
-averages = zeros(15,6);
+averages = zeros(15,4);
 
 for j = 1:15
-    averages(j,:) = results{i+1,j};
+    averages(j,:) = results{i,j};
 end
 
 
 % scatter3(averages(:,1), averages(:,2), averages(:,3));
 % xlabel('alpha'); ylabel('beta'); zlabel('gamma');
 
-plot(averages);
+plot(averages(:,4));
 % figure;
 % plot(averages(:,[2]));
 
