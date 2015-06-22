9fe50086e02957c122bfa403965d91d703ff5bef
Eren Sezener <erensezener@gmail.com>
2015-06-23 01:33:47 +0300

    fix sampling

--- scripts/whole_body_data/multiple_ws/get_metaparameters_with_q.m
+++ scripts/whole_body_data/multiple_ws/get_metaparameters_with_q.m
@@ -8,14 +8,14 @@ beta_range = [-3, 1.5];
 weight_range = [0.1, 0.99];
 
 if trial_no == 1
-    number_of_iterations = 50000;
-    step_size = 0.01;
+    number_of_iterations = 30000;
+    step_size = 0.05;
 else
-    number_of_iterations = 10000;
-    step_size = 0.01;
+    number_of_iterations = 5000;
+    step_size = 0.05;
 end
 
-history = zeros(number_of_iterations + 1, 4);
+history = zeros(number_of_iterations, 4);
 
 % get range lengths
 alpha_range_length = (alpha_range(2) - alpha_range(1));
@@ -44,8 +44,6 @@ else
     weights = initial_weights;
 end
 
-history(1,:) = [alpha, beta, gamma, weights];
-
 rs = get_rewards(action_data, state_data, max_states, min_states, max_actions, min_actions, weights);
 
 [likelihood, Q] = get_likelihood_and_q(as, rs, ss, alpha, exp(beta), gamma, initial_Q);
@@ -55,46 +53,49 @@ end
 
 for i = 1:number_of_iterations
     
-    %% Sampling
-    alpha_prime = 2 * alpha_range_length * step_size * (rand(1) - 0.5) + alpha;
-    beta_prime = 2 * beta_range_length * step_size * (rand(1) - 0.5) + beta;
-    gamma_prime = 2 * gamma_range_length * step_size * (rand(1) - 0.5) + gamma;
-    weight_prime = 2 * weight_range_length * step_size * (rand(1) - 0.5) + weights;
-    
-    primes = [alpha_prime, beta_prime, gamma_prime, weight_prime];
-    
-    loop_count = 0;
-    % resample if alpha, beta, or gamma fall out of range
-    while nnz(primes > lower_bounds) < 4 || nnz(primes < upper_bounds) < 4
+    while 1 % sample until finding a good update
+        %% Sampling
         alpha_prime = 2 * alpha_range_length * step_size * (rand(1) - 0.5) + alpha;
         beta_prime = 2 * beta_range_length * step_size * (rand(1) - 0.5) + beta;
         gamma_prime = 2 * gamma_range_length * step_size * (rand(1) - 0.5) + gamma;
         weight_prime = 2 * weight_range_length * step_size * (rand(1) - 0.5) + weights;
         
         primes = [alpha_prime, beta_prime, gamma_prime, weight_prime];
-        if loop_count > 500
-            display('oops');
-            return
+        
+        loop_count = 0;
+        % resample if alpha, beta, or gamma fall out of range
+        while nnz(primes > lower_bounds) < 4 || nnz(primes < upper_bounds) < 4
+            alpha_prime = 2 * alpha_range_length * step_size * (rand(1) - 0.5) + alpha;
+            beta_prime = 2 * beta_range_length * step_size * (rand(1) - 0.5) + beta;
+            gamma_prime = 2 * gamma_range_length * step_size * (rand(1) - 0.5) + gamma;
+            weight_prime = 2 * weight_range_length * step_size * (rand(1) - 0.5) + weights;
+            
+            primes = [alpha_prime, beta_prime, gamma_prime, weight_prime];
+            if loop_count > 500
+                display('oops');
+                return
+            end
         end
+        
+        %% Accept or Reject
+        rs = get_rewards(action_data, state_data, max_states, min_states, max_actions, min_actions, weights);
+        [likelihood_prime, Q_prime] = get_likelihood_and_q(as, rs, ss, primes(1), exp(primes(2)),primes(3), initial_Q);
+        if isnan(likelihood_prime)
+            display('nan');
+        end
+        
+        if likelihood_prime < likelihood || rand(1) < exp(likelihood - likelihood_prime)
+            alpha = alpha_prime;
+            beta = beta_prime;
+            gamma = gamma_prime;
+            likelihood = likelihood_prime;
+            weights = weight_prime;
+            Q = Q_prime;
+            break;
+        end
+        
     end
-    
-    %% Accept or Reject
-    rs = get_rewards(action_data, state_data, max_states, min_states, max_actions, min_actions, weights);
-    [likelihood_prime, Q_prime] = get_likelihood_and_q(as, rs, ss, primes(1), exp(primes(2)),primes(3), initial_Q);
-    if isnan(likelihood_prime)
-        display('nan');
-    end
-    
-    if likelihood_prime < likelihood || rand(1) < exp(likelihood - likelihood_prime)
-        alpha = alpha_prime;
-        beta = beta_prime;
-        gamma = gamma_prime;
-        likelihood = likelihood_prime;
-        weights = weight_prime;
-        Q = Q_prime;
-    end
-    
-    history(i+1,4) = [alpha, beta, gamma, weights];
+    history(i,:) = [alpha, beta, gamma, weights];
 end
 
 end
