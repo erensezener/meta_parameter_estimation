85ed215fcf5bc110ca615ab35e598f1136b38b13
Eren Sezener <erensezener@gmail.com>
2015-09-29 06:41:40 +0200

    fix a bug

--- scripts/whole_body_data/slice_sampler_only_w/get_likelihood_outer_with_constraints.m
+++ scripts/whole_body_data/slice_sampler_only_w/get_likelihood_outer_with_constraints.m
@@ -11,7 +11,7 @@ a_range = [-1, 1];
 b_range = [0, 1];
 a_plus_b_range = [0.01, 0.99];
 
-w_extended = [w, w(4) + w(5)];
+w_extended = [w, w(1) + w(2)];
 
 lower_bounds = [a_range(1), b_range(1), a_plus_b_range(1)];
 upper_bounds = [a_range(2), b_range(2), a_plus_b_range(2)];
@@ -20,7 +20,7 @@ if nnz(w_extended > lower_bounds) < size(w_extended,2) || nnz(w_extended < upper
     log_likelihood = -1 * realmax;
 else
     log_likelihood = -1 * get_likelihood_and_q(as, ...
-    get_rewards(action_data, state_data, max_states, min_states, max_actions, min_actions, [w(4),w(5)]), ...
+    get_rewards(action_data, state_data, max_states, min_states, max_actions, min_actions, [w(1),w(2)]), ...
     ss, alpha, exp(beta), gamma, initial_Q);
 end
 
